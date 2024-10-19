bced90627bbca03378aae64c40cf33f6c04c7625
dattaram <dmane-consultant@scholastic.com>
2024-10-19 09:40:55 -0400

    example-3: function to generate deck of cards

--- ch0/ch0-example3.py
+++ ch0/ch0-example3.py
@@ -1,13 +1,15 @@
 import random # library for randomness
-suits = ["Clubs", "Spades", "Hearts", "Diamonds"] # Array of suites (Python LIST)
-faces = ["Jack", "Queen", "King", "Ace"] # Array of faces
-numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10] # Array of numbers
-deck = set()
-for suit in suits:
-    for card in faces + numbered:
-        deck.add((card, "of", suit))    # unique tuples added to deck
+def deck_of_cards():
+    suits = ["Clubs", "Spades", "Hearts", "Diamonds"] # Array of suites (Python LIST)
+    faces = ["Jack", "Queen", "King", "Ace"] # Array of faces
+    numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10] # Array of numbers
+    deck = set()
+    for suit in suits:
+        for card in faces + numbered:
+            deck.add((card, "of", suit))    # unique tuples added to deck
+    return deck
 
-print(len(deck)) # length o decl
-for deck in deck:
+print(len(deck_of_cards())) # length o decl
+for deck in deck_of_cards():
     print(deck)
 
